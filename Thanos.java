154b4fe038ab1ce409d4ea6bd440afaf861d8558
jerome valette <valettejerome31@gmail.com>
2019-10-10 10:56:55 +0200

    Finish

--- Thanos.java
+++ Thanos.java
@@ -3,10 +3,18 @@ import java.util.*;
 public class Thanos {
 
     public static void main(String[] args) {
-
         // TODO 1 : Create an empty heroes list
+        ArrayList<Hero> heroes = new ArrayList<>();
 
-        // TODO 2 : Add those heroes to the list
+        Hero hero1 = new Hero("Black Widow",34);
+        Hero hero2 = new Hero("Captain America",100);
+        Hero hero3 = new Hero("Vision",3);
+        Hero hero4 = new Hero("Iron Man",48);
+        Hero hero5 = new Hero("Scarlet Witch",29);
+        Hero hero6 = new Hero("Thor",1500);
+        Hero hero7 = new Hero("Spider-Man",18);
+        Hero hero8 = new Hero("Hulk",49);
+        Hero hero9 = new Hero("Doctor Strange",42);
         // name: Black Widow, age: 34
         // name: Captain America, age: 100
         // name: Vision, age: 3
@@ -17,12 +25,36 @@ public class Thanos {
         // name: Hulk, age: 49
         // name: Doctor Strange, age: 42
 
+
+        // TODO 2 : Add those heroes to the list
+        heroes.add(hero1);
+        heroes.add(hero2);
+        heroes.add(hero3);
+        heroes.add(hero4);
+        heroes.add(hero5);
+        heroes.add(hero6);
+        heroes.add(hero7);
+        heroes.add(hero8);
+        heroes.add(hero9);
         // TODO 3 : It's Thor birthday, now he's 1501
 
+        hero6.setAge(1501);
+        heroes.set(5,hero6);
+
         // TODO 4 : Shuffle the heroes list
+        Collections.shuffle(heroes);
 
         // TODO 5 : Keep only the half of the list
+        heroes.remove(1);
+        heroes.remove(2);
+        heroes.remove(3);
+        heroes.remove(4);
+        
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for (Hero h: heroes) {
+            System.out.println(h.getName() + " "+ h.getAge());
+        }
     }
+
 }
